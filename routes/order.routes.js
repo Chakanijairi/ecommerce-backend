b7da3caf627ec6e0ed48318f40bb0ef60806da45
Lawliet <chawkanijairi8@gmail.com>
2025-11-07 23:16:44 +0800

    Update backend

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { createCheckout, getAllOrder } = require('../controller/order.controller')
+const { authenticate, authorizeRoles } = require('../middleware/auth.middleware');
 
 const router = express.Router();
 
-router.post('/createCheckout', createCheckout)
-router.get('/orders', getAllOrder)
+router.post('/createCheckout', authenticate, createCheckout)
+router.get('/orders', authenticate, authorizeRoles('Admin'), getAllOrder)
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,38 +1,45 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config(); 
+const path = require("path");
+require("dotenv").config();
 const app = express();
-const orderRoutes = require('./routes/order.routes')
+const orderRoutes = require("./routes/order.routes");
+const userRoutes = require("./routes/auth.routes");
+const productRoutes = require("./routes/product.routes");
 
-const connectDb = require('./config/db')
+const connectDb = require("./config/db");
 
 const allowedOrigins = [
-  "https://ecommerce-frontend-ruddy-kappa.vercel.app", // your deployed frontend
-  "http://localhost:5173" // your local frontend for testing
+  "https://ecommerce-frontend-ruddy-kappa.vercel.app",
+  "http://localhost:5173",
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true, // optional if you're using cookies or authentication
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
-app.use(express.json()); 
+app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 
-app.use('/api/order', orderRoutes)
-
-app.get('/', (req, res) => {
-  res.send("Hello world")
-})
+app.use("/api/order", orderRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/products", productRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 app.listen(PORT, () => {
   connectDb();
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`);
 });
